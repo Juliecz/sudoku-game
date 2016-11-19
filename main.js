1d365b39a53162fd1c25e6e6928b52557ed413be
Julie <julieregistr@gmail.com>
2016-11-19 03:05:21 +0100

    Small table error

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 angular.module('sudokugame', [])
     .controller('mainCtrl', ['$scope', function ($scope) {
         $scope.grid = [[],[],[],[],[],[],[],[],[]];
-        $scope.selectedClass = '';
         $scope.possibleValue = [1,2,3,4,5,6,7,8,9];
         $scope.selectedCell = {
             selected: false,
@@ -90,7 +89,8 @@ angular.module('sudokugame', [])
                 for (var j = 0; j < 9; j++) {
                     novePole[i][j] = {
                         number: pole[i][j],
-                        edit: false
+                        edit: false,
+                        visNumbers: false
                     }
                 }
             }
@@ -105,13 +105,15 @@ angular.module('sudokugame', [])
             return novePole;
         }
         $scope.selected = function (x, y, ifselected) {
+            // unselect
             if (ifselected) {
                 $scope.selectedCell = {
                     selected: false,
                     x: null,
                     y: null
-                }
+                };
             }
+            //select
             else {
                 $scope.selectedCell = {
                     selected: true,
@@ -120,35 +122,41 @@ angular.module('sudokugame', [])
                 };
             }
         };
+
         $scope.chooseValue = function(num) {
-            if ($scope.selectedCell.selected) {
-                $scope.pole[$scope.selectedCell.y][$scope.selectedCell.x].number = num;
-                $scope.selectedCell = {
-                    selected: false,
-                    x: null,
-                    y: null
-                };
-            }
+            $scope.pole[$scope.selectedCell.y][$scope.selectedCell.x].number = num;
+            $scope.selected(null, null, true);
+            /*$scope.selectedCell = {
+             selected: false,
+             x: null,
+             y: null
+             };*/
+            console.log($scope.selectedCell);
         };
         $scope.restart = function () {
-            for (i=0; i<9; i++) {
+            for (var i=0; i<9; i++) {
                 for (var j = 0; j < 9; j++) {
                     if ($scope.pole[i][j].edit) {
                         $scope.pole[i][j].number = null;
+                        $scope.pole[i][j].class = null;
                     }
                 }
             }
+            $scope.selectedCell = {
+                selected: false,
+                x: null,
+                y: null
+            };
         };
         $scope.solution = function () {
-            for (i=0; i<9; i++) {
+            for (var i=0; i<9; i++) {
                 for (var j = 0; j < 9; j++) {
-                    $scope.pole[i][j].number = $scope.grid[i][j];
+                    if ($scope.pole[i][j].number !== $scope.grid[i][j]) {
+                        $scope.pole[i][j].class = 'warning';
+                        $scope.pole[i][j].number = $scope.grid[i][j];
+                    }
                 }
             }
-        };
-        $scope.keyboardInput = function () {
-            console.log('lkdfj');
-            //$scope.pole[$scope.selectedCell.y][$scope.selectedCell.x]
         };
         $scope.generate();
     }]);
